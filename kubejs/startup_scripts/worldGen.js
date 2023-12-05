8db1ac7a177aaaa31bc92011ace3921ee47bdd19
xImBlank <xImBlank@outlook.com>
2023-12-05 09:27:03 +0100

    oregen test

--- kubejs/startup_scripts/worldGen.js
+++ kubejs/startup_scripts/worldGen.js
@@ -1,10 +1,6 @@
 WorldgenEvents.add(event => {
   event.removeOres(props => {
-   
-    props.blocks = [/ore\b/]
+    props.blocks = ['minecraft:iron_ore', 'minecraft:copper_ore']
   })
 })
-
-
-a*b
 
