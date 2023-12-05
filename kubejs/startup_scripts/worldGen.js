fe78e94a6407308788a877fb86b190da4a063807
Fabian Schacht <mcfer2004@gmail.com>
2023-12-05 09:33:30 +0100

    remove

--- kubejs/startup_scripts/worldGen.js
+++ kubejs/startup_scripts/worldGen.js
@@ -1,12 +1,6 @@
-WorldgenEvents.add(event => {
+WorldgenEvents.remove(event => {
   event.removeOres(props => {
     props.blocks = ['minecraft:iron_ore', 'minecraft:copper_ore']
   })
 })
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 8db1ac7a177aaaa31bc92011ace3921ee47bdd19
