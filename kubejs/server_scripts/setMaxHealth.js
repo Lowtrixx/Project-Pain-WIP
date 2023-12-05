31a1e1ca342a061cef13e98098b34907f2da2ecb
xImBlank <xImBlank@outlook.com>
2023-12-05 02:12:41 +0100

    123

--- kubejs/server_scripts/setMaxHealth.js
+++ kubejs/server_scripts/setMaxHealth.js
@@ -4,9 +4,11 @@ if (!e.player.stages.has('starting_items')) {
     e.player.stages.add('starting_items')
     e.player.give('30x minecraft:apple')
     e.player.setMaxHealth(6)
+    e.player.setFoodLevel(12)
 }
 })
 PlayerEvents.respawned(e=> {
     if(!e.player.getMaxHealth==6)
     e.player.setMaxHealth(e.player.getMaxHealth-14)
+    e.player.setFoodLevel(e.player.getFoodLevel-8)
    })
