31af4833099789816287fc69e6fc99522eee1d30
Fabian Schacht <mcfer2004@gmail.com>
2023-12-05 09:44:17 +0100

    testing

--- kubejs/startup_scripts/worldGen.js
+++ kubejs/startup_scripts/worldGen.js
@@ -1,5 +1,6 @@
 WorldgenEvents.remove(event => {
   event.removeOres(props => {
+    props.worldgenLayer = 'underground_ores'
     props.blocks = ['minecraft:iron_ore', 'minecraft:copper_ore']
   })
 })
